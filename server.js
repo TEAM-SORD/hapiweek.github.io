896f1fb7a9ef9270788db6aa5bf0e81d94376d69
beechware <davebeechmovement@yahoo.co.uk>
2015-03-17 15:02:25 +0000

    20150317 15:02

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
+var postsColl = require("./posts");
 var Hapi = require('hapi');
+// var mongoose = require("mongoose");
+// var uriUtil = require('mongodb-uri');
 
 var server = new Hapi.Server();
-server.connection({ port: 8000 });
+server.connection({ port: 3000 });
 
-// A set of example RESTful routes that could describe a blog
+var joiSchema = Joi.object().options({ abortEarly: false }).keys({
+    author: Joi.string().required(),
+    title: Joi.string().required(),
+    text: Joi.string().required(),
+    date: Joi.date().required(),
+    image: Joi.string().optional()
+});
 
+server.register([ /* plugins */], function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugins
+    }
 
-server.route({
+    // ... Register the routes
+    server.route({
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
-        reply('Blog homepage here');
+        var query = postsColl.getPosts( );
+        query.sort( {date: 'descending'} ).exec( function (err, posts) { 
+            console.log( 'In getPostsForSideBar - query.exec : ' + posts ); 
+            // allPosts = posts; renderOtherPane( allPosts ); 
+            reply('Blog homepage here' + posts.length);
+        });
+            // function(err, blogPosts) {
+        //     console.log( 'In blogPostModel : ' + blogPosts);
+        //     if( err ) {
+        //         console.log( 'Error: ' + err )
+
+        
     }
 });
 
@@ -42,12 +67,23 @@ server.route({
 // payload parse 'true' is the default value, but worth knowing about. Uses the content-type header to parse the payload. set to false if you want the raw payload.
 server.route({
     method: 'POST',
-    config: { payload: {output: 'data', parse: true} },
+    config: { 
+        validate:{
+            payload: joiSchema,
+        },
+        payload: {output: 'data', parse: true} 
+    },
     path: '/',
     handler: function (request, reply) {
         // code here to handle new post
-        reply('New Post Added');
+        // request.payload.author;
+        postsColl.addPost("a post to add to the database-json object", function (err){
+                console.log( ( err )? 'Error posting to db: ' + err : 'Successfully added to db');
+             reply('New Post Added');   
+            });
+        
     }
+
 });
 
 // PUT has a payload too..
@@ -74,3 +110,16 @@ server.route({
 server.start(function () {
     console.log('Server running at:', server.info.uri);
 });
+
+    server.start(function () {
+        // mongoose.connect(mongooseUri, function(err) {
+            // if (err) console.log('error', err);
+            // console.log("connected");
+        // });
+    });
+
+});
+
+// A set of example RESTful routes that could describe a blog
+
+
